4b5872de85ed85e2c3c7a29aa014dde63e14c9db
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2020-06-08 10:59:11 +0200

    vimrc

--- 01-install.sh
+++ 01-install.sh
@@ -1,10 +1,11 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install git zsh curl -y
+sudo apt install git zsh curl vim -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp zshrc ~/.zshrc
+cp generate.vim ~/.vimrc
 
 source ~/.zshrc
