fdfcdc609072b8c48f103972c4eabb8859eae26e
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2020-06-08 10:52:02 +0200

    -y to apt

--- 01-install.sh
+++ 01-install.sh
@@ -1,4 +1,10 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install git zsh curl
+sudo apt install git zsh curl -y
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp zshrc ~/.zshrc
+
+source ~/.zshrc
